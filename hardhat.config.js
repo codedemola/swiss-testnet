7e008c18bd847943804085f4f834426421108bec
codedemola <connectwithjade24@gmail.com>
2024-07-27 09:44:53 +0000

    swiss comit

---
+++ hardhat.config.js
@@ -0,0 +1,11 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    swisstronik: {
+      url: "https://json-rpc.testnet.swisstronik.com/",
+      accounts: ["0xDB2888AB15c9d37c9a0086F426623F857B03B063"],
+    },
+  },
+};
